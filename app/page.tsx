836e9cb00f257d7ffba3e89a94dab31acd19f3cd
Mahnoor <mahnoort104@gmail.com>
2024-12-10 03:13:02 +0500

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import FeaturedProductsPage from "./components/featured";
 import HeroPage from "./components/hero";
 import OurProduct from "./components/ourProducts";
-
+import ExploreProduct from "./components/exploreProducts";
 
 export default function Home() {
   return (
   <div>
     <HeroPage/>
     <FeaturedProductsPage/>
+     <ExploreProduct/>
     <OurProduct/>
   </div>
   );
